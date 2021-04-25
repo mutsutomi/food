22b6fccfd03757779693610933940453623e708c
mutsutomi <m2.tmk@ezweb.ne.jp>
2021-04-26 03:10:31 +0900

    4/26fix105

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,33 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
-
+import useResults from "../hooks/useResults";
 
 const SearchScreen=()=>{
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const searchApi= async (searchTerm)=>{
-    try {
-      const response= await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: "san jose"
-        }
-      });
-      setResults(response.data.businesses);
-    } catch(err){
-      // console.log(err)
-      setErrorMessage("Something went wrong");
-    }
-  };  
-
-useEffect(()=>{
-  searchApi("pasta");
-},[]);
+  const [searchApi, results, errorMessage]=useResults();
 
   return (
     <View>
@@ -37,7 +15,7 @@ useEffect(()=>{
       onTermSubmit={()=>searchApi(term)}  
     />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {results.length} results</Text>
+      <Text>We have found {results.length} results 105</Text>
     </View>
   );
 };
